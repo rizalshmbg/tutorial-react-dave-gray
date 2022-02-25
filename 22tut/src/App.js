a8aeb7d93a1b82160086dee045146f8c3cbb75e1
rizalshmbg <sihombingrizal18@gmail.com>
2022-02-25 09:57:39 +0700

    completed chapter 22

---
+++ 22tut/src/App.js
@@ -0,0 +1,46 @@
+import Header from './Header';
+import Nav from './Nav';
+import Footer from './Footer';
+import Home from './Home';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import EditPost from './EditPost';
+import About from './About';
+import Missing from './Missing';
+import { useEffect } from 'react';
+import useAxiosFetch from './hooks/useAxiosFetch';
+import { Route, Switch } from 'react-router-dom';
+import { action, useStoreActions } from 'easy-peasy';
+
+function App() {
+
+  const setPosts = useStoreActions((action) => action.setPosts);
+  const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts');
+
+  useEffect(() => {
+    setPosts(data);
+  }, [data, setPosts]);
+
+  return (
+    <div className="App">
+      <Header title="React JS Blog" />
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Home
+            isLoading={isLoading}
+            fetchError={fetchError}
+          />
+        </Route>
+        <Route exact path="/post" component={NewPost} />
+        <Route path="/edit/:id" component={EditPost} />
+        <Route path="/post/:id" component={PostPage} />
+        <Route path="/about" component={About} />
+        <Route path="*" component={Missing} />
+      </Switch>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
